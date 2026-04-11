a9ebc5a2ad3cb77a6380407b2fc4f6380893f235
LionnoiL <wmconstructor@gmail.com>
2026-04-11 12:25:17 +0300

    add new codes for event names

--- src/main/java/ua/haponov/LogMigrationService.java
+++ src/main/java/ua/haponov/LogMigrationService.java
@@ -18,30 +18,107 @@ import java.util.Map;
 public class LogMigrationService {
 
     private static final Map<String, String> EVENT_NAME_MAP = Map.ofEntries(
-            Map.entry("_$Data$_.New", "Создание"),
-            Map.entry("_$Data$_.Update", "Изменение"),
-            Map.entry("_$Data$_.Delete", "Удаление"),
-            Map.entry("_$Data$_.Post", "Проведение"),
-            Map.entry("_$Data$_.Unpost", "Отмена проведения"),
+            // --- ДОСТУП ---
+            Map.entry("_$Access$_.Access", "Доступ. Доступ"),
+            Map.entry("_$Access$_.AccessDenied", "Доступ. Отказ в доступе"),
+
+            // --- ДАННЫЕ (Data) ---
+            Map.entry("_$Data$_.New", "Данные. Добавление"),
+            Map.entry("_$Data$_.Update", "Данные. Изменение"),
+            Map.entry("_$Data$_.Delete", "Данные. Удаление"),
+            Map.entry("_$Data$_.Post", "Данные. Проведение"),
+            Map.entry("_$Data$_.Unpost", "Данные. Отмена проведения"),
+            Map.entry("_$Data$_.NewPredefinedData", "Данные. Добавление предопределенных данных"),
+            Map.entry("_$Data$_.UpdatePredefinedData", "Данные. Изменение предопределенных данных"),
+            Map.entry("_$Data$_.DeletePredefinedData", "Данные. Удаление предопределенных данных"),
+            Map.entry("_$Data$_.NewVersion", "Данные. Добавление версии"),
+            Map.entry("_$Data$_.DeleteVersions", "Данные. Удаление версий"),
+            Map.entry("_$Data$_.VersionCommentUpdate", "Данные. Изменение комментария версии"),
+            Map.entry("_$Data$_.PredefinedDataInitialization", "Данные. Инициализация предопределенных данных"),
+            Map.entry("_$Data$_.PredefinedDataInitializationDataNotFound", "Данные. Инициализация предопределенных данных (не найдены)"),
+            Map.entry("_$Data$_.SetPredefinedDataInitialization", "Данные. Установка инициализации предопределенных данных"),
+            Map.entry("_$Data$_.SetStandardODataInterfaceContent", "Данные. Изменение состава OData"),
+            Map.entry("_$Data$_.TotalsMaxPeriodUpdate", "Данные. Изменение макс. периода итогов"),
+            Map.entry("_$Data$_.TotalsMinPeriodUpdate", "Данные. Изменение мин. периода итогов"),
+
+            // --- ИНФОРМАЦИОННАЯ БАЗА (InfoBase) ---
+            Map.entry("_$InfoBase$_.ConfigUpdate", "ИБ. Изменение конфигурации"),
+            Map.entry("_$InfoBase$_.DBConfigUpdate", "ИБ. Изменение конфигурации БД"),
+            Map.entry("_$InfoBase$_.DBConfigUpdateStart", "ИБ. Запуск обновления конфигурации БД"),
+            Map.entry("_$InfoBase$_.DBConfigUpdateError", "ИБ. Ошибка обновления конфигурации БД"),
+            Map.entry("_$InfoBase$_.ConfigExtensionUpdate", "ИБ. Изменение расширения конфигурации"),
+            Map.entry("_$InfoBase$_.DBConfigExtensionUpdate", "ИБ. Изменение расширения в БД"),
+            Map.entry("_$InfoBase$_.DBConfigExtensionUpdateError", "ИБ. Ошибка изменения расширения в БД"),
+            Map.entry("_$InfoBase$_.DBConfigBackgroundUpdateStart", "ИБ. Запуск фонового обновления БД"),
+            Map.entry("_$InfoBase$_.DBConfigBackgroundUpdateFinish", "ИБ. Завершение фонового обновления БД"),
+            Map.entry("_$InfoBase$_.DBConfigBackgroundUpdateCancel", "ИБ. Отмена фонового обновления БД"),
+            Map.entry("_$InfoBase$_.DBConfigBackgroundUpdateResume", "ИБ. Продолжение фонового обновления БД"),
+            Map.entry("_$InfoBase$_.DBConfigBackgroundUpdateSuspend", "ИБ. Пауза фонового обновления БД"),
+            Map.entry("_$InfoBase$_.EventLogSettingsUpdate", "ИБ. Изменение параметров журнала регистрации"),
+            Map.entry("_$InfoBase$_.EventLogSettingsUpdateError", "ИБ. Ошибка настройки журнала регистрации"),
+            Map.entry("_$InfoBase$_.EventLogReduce", "ИБ. Сокращение журнала регистрации"),
+            Map.entry("_$InfoBase$_.EventLogReduceError", "ИБ. Ошибка сокращения журнала регистрации"),
+            Map.entry("_$InfoBase$_.DumpStart", "ИБ. Начало выгрузки в файл"),
+            Map.entry("_$InfoBase$_.DumpFinish", "ИБ. Окончание выгрузки в файл"),
+            Map.entry("_$InfoBase$_.DumpError", "ИБ. Ошибка выгрузки в файл"),
+            Map.entry("_$InfoBase$_.RestoreStart", "ИБ. Начало загрузки из файла"),
+            Map.entry("_$InfoBase$_.RestoreFinish", "ИБ. Окончание загрузки из файла"),
+            Map.entry("_$InfoBase$_.RestoreError", "ИБ. Ошибка загрузки из файла"),
+            Map.entry("_$InfoBase$_.ParametersUpdate", "ИБ. Изменение параметров ИБ"),
+            Map.entry("_$InfoBase$_.ParametersUpdateError", "ИБ. Ошибка изменения параметров ИБ"),
+            Map.entry("_$InfoBase$_.MasterNodeUpdate", "ИБ. Изменение главного узла"),
+            Map.entry("_$InfoBase$_.PredefinedDataUpdate", "ИБ. Обновление предопределенных данных"),
+            Map.entry("_$InfoBase$_.RegionalSettingsUpdate", "ИБ. Изменение региональных установок"),
+            Map.entry("_$InfoBase$_.EraseData", "ИБ. Удаление данных ИБ"),
+            Map.entry("_$InfoBase$_.AdditionalAuthenticationSettingsUpdate", "ИБ. Изменение настроек аутентификации"),
+            Map.entry("_$InfoBase$_.SecondAuthenticationFactorTemplateNew", "ИБ. Добавление шаблона 2-го фактора"),
+            Map.entry("_$InfoBase$_.SecondAuthenticationFactorTemplateUpdate", "ИБ. Изменение шаблона 2-го фактора"),
+            Map.entry("_$InfoBase$_.SecondAuthenticationFactorTemplateDelete", "ИБ. Удаление шаблона 2-го фактора"),
+            Map.entry("_$InfoBase$_.IntegrationServiceActiveUpdate", "ИБ. Изменение активности сервиса интеграции"),
+            Map.entry("_$InfoBase$_.IntegrationServiceSettingsUpdate", "ИБ. Изменение настроек сервиса интеграции"),
+
+            // --- ТЕСТИРОВАНИЕ И ИСПРАВЛЕНИЕ ---
+            Map.entry("_$InfoBase$_.VerifyAndRepairInfo", "Тестирование и исправление. Сообщение"),
+            Map.entry("_$InfoBase$_.VerifyAndRepairMessage", "Тестирование и исправление. Предупреждение"),
+            Map.entry("_$InfoBase$_.VerifyAndRepairImportant", "Тестирование и исправление. Ошибка"),
+
+            // --- ФОНОВЫЕ ЗАДАНИЯ (Jobs) ---
+            Map.entry("_$Job$_.Start", "Фоновое задание. Запуск"),
+            Map.entry("_$Job$_.Finish", "Фоновое задание. Завершение."),
+            Map.entry("_$Job$_.Succeed", "Фоновое задание. Успешное завершение"),
+            Map.entry("_$Job$_.Fail", "Фоновое задание. Ошибка выполнения"),
+            Map.entry("_$Job$_.Error", "Фоновое задание. Ошибка (системная)"),
+            Map.entry("_$Job$_.Cancel", "Фоновое задание. Отмена"),
+            Map.entry("_$Job$_.Terminate", "Фоновое задание. Принудительное завершение"),
+
+            // --- СЕАНСЫ (Sessions) ---
+            Map.entry("_$Session$_.Start", "Сеанс. Начало"),
+            Map.entry("_$Session$_.Finish", "Сеанс. Завершение"),
+            Map.entry("_$Session$_.Authentication", "Сеанс. Аутентификация"),
+            Map.entry("_$Session$_.AuthenticationError", "Сеанс. Ошибка аутентификации"),
+            Map.entry("_$Session$_.AuthenticationFirstFactor", "Сеанс. Аутентификация (1-й фактор)"),
+            Map.entry("_$Session$_.ConfigExtensionApplyError", "Сеанс. Ошибка применения расширения"),
+
+            // --- ТРАНЗАКЦИИ ---
             Map.entry("_$Transaction$_.Begin", "Транзакция. Начало"),
             Map.entry("_$Transaction$_.Commit", "Транзакция. Фиксация"),
             Map.entry("_$Transaction$_.Rollback", "Транзакция. Отмена"),
-            Map.entry("_$Session$_.Start", "Сеанс. Начало"),
-            Map.entry("_$Session$_.Finish", "Сеанс. Завершение"),
-            Map.entry("_$Session$_.Authentication", "Аутентификация"),
-            Map.entry("_$Session$_.AuthenticationError", "Ошибка аутентификации"),
-            Map.entry("_$Job$_.Start", "Фоновое задание. Запуск"),
-            Map.entry("_$Job$_.Succeed", "Фоновое задание. Успешное завершение"),
-            Map.entry("_$Job$_.Error", "Фоновое задание. Ошибка выполнения"),
-            Map.entry("_$Job$_.Terminate", "Фоновое задание. Отмена"),
-            Map.entry("_$User$_.New", "Пользователь. Новый"),
-            Map.entry("_$User$_.Update", "Пользователь. Изменение"),
-            Map.entry("_$User$_.Delete", "Пользователь. Удаление"),
-            Map.entry("_$Config$_.Update", "Изменение конфигурации"),
-            Map.entry("_$Metadata$_.Update", "Изменение метаданных"),
+
+            // --- ПОЛЬЗОВАТЕЛИ ---
+            Map.entry("_$User$_.New", "Пользователи. Добавление"),
+            Map.entry("_$User$_.Update", "Пользователи. Изменение"),
+            Map.entry("_$User$_.Delete", "Пользователи. Удаление"),
+            Map.entry("_$User$_.NewError", "Пользователи. Ошибка добавления"),
+            Map.entry("_$User$_.UpdateError", "Пользователи. Ошибка изменения"),
+            Map.entry("_$User$_.DeleteError", "Пользователи. Ошибка удаления"),
+            Map.entry("_$User$_.AuthenticationLock", "Пользователи. Блокировка аутентификации"),
+            Map.entry("_$User$_.AuthenticationUnlock", "Пользователи. Разблокировка аутентификации"),
+            Map.entry("_$User$_.AuthenticationUnlockError", "Пользователи. Ошибка разблокировки"),
+
+            // --- ПРОЧЕЕ ---
             Map.entry("_$PerformError$_", "Ошибка выполнения"),
-            Map.entry("_$InfoBase$_.EventLogSettingsUpdateError", "Ошибка настройки журнала регистрации"),
-            Map.entry("_$InfoBase$_.DBConfigUpdateError", "Ошибка обновления конфигурации БД")
+            Map.entry("_$OpenIDProvider$_.PositiveAssertion", "OpenID. Подтверждено"),
+            Map.entry("_$OpenIDProvider$_.NegativeAssertion", "OpenID. Отклонено")
     );
 
     private static final long TICKS_AT_EPOCH = 62135596800L;
